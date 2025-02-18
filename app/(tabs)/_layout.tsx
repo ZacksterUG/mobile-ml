3a646181d344f9adca5ce0606f3f87dabec5f86b
ZacksterUG <fomkalex@mail.ru>
2025-02-18 10:55:08 +0500

    Added index

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
 import { Tabs } from "expo-router";
 
+
 export default function TabLayout() {
   return (
   <Tabs>
+    <Tabs.Screen name="index" options={{ title: 'Главная' }}/>
     <Tabs.Screen name="image_input" options={{ title: 'Загрузить' }}/>
     <Tabs.Screen name="drawing_screen" options={{ title: 'Порисовать' }}/>
   </Tabs>
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router";
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, PanResponder, Alert } from 'react-native';
+
+export default function index() {
+    return (
+      <View>
+      </View>
+    )
+}
